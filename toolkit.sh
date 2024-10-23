c86676cd55425a635984d7f2947de217c10c8717
Sameer Deshmukh <sameer17cs@gmail.com>
2024-10-23 14:55:42 +0530

    disk format filesystems

--- toolkit.sh
+++ toolkit.sh
@@ -11,49 +11,80 @@ LIB_SCRIPT="_lib.sh"
 
 mount_disk() {
 
+  # Prompt for mount directory and device name
   _prompt_for_input_ MOUNT_DIR "Please enter mount directory full path" true
-
   _prompt_for_input_ DEVICE_NAME "Please enter device name (run lsblk)" true
 
   device_path="/dev/$DEVICE_NAME"
 
-  #Disk Format
-  _prompt_for_input_ FORMAT_RESPONSE "Do you want format the disk (it will wipe out the disk), (Y|y|N|n)" true
+  # Disk Format Option
+  _prompt_for_input_ FORMAT_RESPONSE "Do you want to format the disk (it will wipe out the disk), (Y|y|N|n)" true
 
   if [[ $FORMAT_RESPONSE == "Y" || $FORMAT_RESPONSE == "y" ]]; then
-    sudo mkfs.ext4 -I 128 $device_path 
-    echo -e "${C_YELLOW}Completed disk format ${C_DEFAULT}"
+
+    # Prompt for filesystem type
+    valid_filesystems=("ext4" "xfs" "btrfs")
+    while true; do
+      echo -e "Available filesystems: ${valid_filesystems[@]}"
+      _prompt_for_input_ FILESYSTEM_TYPE "Please enter the filesystem type you want to use" true
+
+      # Check if the entered filesystem type is valid
+      if [[ " ${valid_filesystems[@]} " =~ " ${FILESYSTEM_TYPE} " ]]; then
+        break
+      else
+        echo -e "${C_RED}Invalid filesystem type entered. Please choose from: ${valid_filesystems[@]}${C_DEFAULT}"
+      fi
+    done
+
+    # Format the disk with the selected filesystem
+    case $FILESYSTEM_TYPE in
+      ext4)
+        sudo mkfs.ext4 -I 128 $device_path
+        ;;
+      xfs)
+        sudo mkfs.xfs $device_path
+        ;;
+      btrfs)
+        sudo mkfs.btrfs $device_path
+        ;;
+    esac
+
+    echo -e "${C_YELLOW}Completed disk format with $FILESYSTEM_TYPE${C_DEFAULT}"
   else
-    echo -e "${C_PURPLE} Skipped disk format, Warning: script might fail if disk is not in correct filesystem format..${C_DEFAULT}"
+    echo -e "${C_PURPLE}Skipped disk format. Warning: script might fail if disk is not in correct filesystem format.${C_DEFAULT}"
   fi
-  
-  #Disk Mount
+
+  # Disk Mount
   uuid_for_fstab=$(sudo blkid -s UUID -o value $device_path)
-  echo -e "${C_BLUE} Device UUID: $uuid_for_fstab ${C_DEFAULT}"
+  echo -e "${C_BLUE}Device UUID: $uuid_for_fstab${C_DEFAULT}"
 
   if [ -z "$uuid_for_fstab" ]; then 
-    echo -e "${C_RED} Disk incorrect Or not initialized/formatted properly, exiting..${C_DEFAULT}"
+    echo -e "${C_RED}Disk incorrect or not initialized/formatted properly, exiting...${C_DEFAULT}"
     exit 1
   fi
-  
-  ##Create mount point
+
+  # Create mount point
   sudo mkdir -p $MOUNT_DIR
-  echo -e "${C_BLUE} Created directory at mount point ${C_DEFAULT}"
- 
-  ##mount disk
+  echo -e "${C_BLUE}Created directory at mount point${C_DEFAULT}"
+
+  # Mount disk
   sudo mount $device_path $MOUNT_DIR
+  if [ $? -ne 0 ]; then
+    echo -e "${C_RED}Failed to mount the disk. Exiting...${C_DEFAULT}"
+    exit 1
+  fi
 
-  ##add fstab entry
-  line_for_fstab="UUID=$uuid_for_fstab $MOUNT_DIR ext4 defaults 0 2"
+  # Add fstab entry
+  line_for_fstab="UUID=$uuid_for_fstab $MOUNT_DIR $FILESYSTEM_TYPE defaults 0 2"
   echo -e "$line_for_fstab\n" | sudo tee -a /etc/fstab
-  echo -e "${C_BLUE} Added entry in fstab ${C_DEFAULT}"
-  
-  #Cleanup
-  echo -e "${C_BLUE} Changing directory owner to $USER ${C_DEFAULT}"
-  sudo chown -R $USER:$USER $MOUNT_DIR
-  echo -e "${C_GREEN} Disk Mount success!${C_DEFAULT}"
+  echo -e "${C_BLUE}Added entry in fstab${C_DEFAULT}"
 
-  #Test mount
+  # Cleanup
+  echo -e "${C_BLUE}Changing directory owner to $USER${C_DEFAULT}"
+  sudo chown -R $USER:$USER $MOUNT_DIR
+  echo -e "${C_GREEN}Disk mount success!${C_DEFAULT}"
+
+  # Test mount
   sudo mount -a
 }
 
